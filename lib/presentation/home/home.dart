bcd3ca4232816707f94544e42baa11f4e78a98de
LeonelCamposM <leonelscamposm@gmail.com>
2022-10-04 14:39:20 -0600

    quit debug tag

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -6,6 +6,7 @@ import 'package:spot_it_game/domain/rooms/room.dart';
 import 'package:spot_it_game/infrastructure/rooms/rooms_repository.dart';
 import 'package:spot_it_game/presentation/core/button_style.dart';
 import 'package:spot_it_game/presentation/core/loading_widget.dart';
+import 'package:spot_it_game/presentation/waiting_room/waiting_room.dart';
 
 class RoomPage extends StatefulWidget {
   static String routeName = '/';
@@ -38,7 +39,9 @@ class _RoomPageState extends State<RoomPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 09, 114, 171),
-      appBar: AppBar(title: const Text('Inicio'), backgroundColor: Color.fromARGB(255, 06, 70, 99)),
+      appBar: AppBar(
+          title: const Text('Inicio'),
+          backgroundColor: Color.fromARGB(255, 06, 70, 99)),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
@@ -58,69 +61,72 @@ class _RoomWidget extends StatefulWidget {
 
 class _RoomWidgetState extends State<_RoomWidget> {
   final ClientService clientService = ClientService();
-  final ButtonStyle style = getButtonStyle(650, 85, 30.0, Color.fromARGB(255, 06, 70, 99));
+  final ButtonStyle style =
+      getButtonStyle(650, 85, 30.0, Color.fromARGB(255, 06, 70, 99));
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-
         Flexible(
           flex: 3,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             // ignore: prefer_const_literals_to_create_immutables
             children: [
-                const Image(
-                  image: AssetImage('assets/logo.png'),
+              const Image(
+                image: AssetImage('assets/logo.png'),
+              ),
+              const Text(
+                "Spot it!",
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
                 ),
-                const Text(
-                  "Spot it!",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+              ),
             ],
           ),
         ),
-
         Flexible(
-              flex: 2,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                    style: style,
-                    onPressed: () => clientService.emitCreateRoom(
-                      "user name",
-                    ),
-                    child: const Text('ANFITRIÓN'),
-                  ),
-                  ElevatedButton(
-                    style: style,
-                    onPressed: () {},
-                    child: const Text('INVITADO'),
-                  ),
-                ],
+          flex: 2,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                style: style,
+                onPressed: () => clientService.emitCreateRoom(
+                  "user name",
+                ),
+                child: const Text('ANFITRIÓN'),
               ),
+              ElevatedButton(
+                style: style,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const WaitingRoomPage()),
+                  );
+                },
+                child: const Text('INVITADO'),
+              ),
+            ],
+          ),
         ),
-
         Flexible(
-              flex: 1,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                   IconButton(
-                    iconSize: 35.0,
-                    icon: const Icon(Icons.receipt),
-                    color: Color.fromARGB(255, 06, 70, 99),
-                    onPressed: () async {
-                    },
-                  ),
-                ],
+          flex: 1,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              IconButton(
+                iconSize: 35.0,
+                icon: const Icon(Icons.receipt),
+                color: Color.fromARGB(255, 06, 70, 99),
+                onPressed: () async {},
               ),
+            ],
+          ),
         ),
       ],
     );
