faff23ba26c947c1edbee9aa484e0b8289dae649
Sofia-29 <angiesofiag04@gmail.com>
2022-10-07 17:46:10 -0600

    Add refactoring to game code and some changes to waiting room

---
+++ lib/presentation/game/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color getPrimaryColor() {
+  return const Color.fromARGB(255, 156, 33, 201);
+}
+
+Color getSecondaryColor() {
+  return const Color.fromARGB(255, 109, 31, 138);
+}
--- lib/presentation/game/game.dart
+++ lib/presentation/game/game.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:spot_it_game/presentation/chat/chat.dart';
+import 'package:spot_it_game/presentation/core/icon_button_style.dart';
+import 'package:spot_it_game/presentation/core/text_style.dart';
+import 'package:spot_it_game/presentation/game/colors.dart';
 import 'package:spot_it_game/presentation/home/home.dart';
 
 
@@ -11,7 +15,6 @@ class GamePage extends StatefulWidget {
 
 class _GamePagePageState extends State<GamePage> {
   _GamePagePageState() : isLoading = true;
-
   bool isLoading;
 
   @override
@@ -22,94 +25,16 @@ class _GamePagePageState extends State<GamePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(100, 156, 33, 201),
+      backgroundColor: getPrimaryColor(),
       appBar: AppBar(
         title: const Text('Juego'),
         automaticallyImplyLeading: false,
-        backgroundColor: const Color.fromARGB(100, 109, 31, 138),
+        backgroundColor: getSecondaryColor(),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    IconButton(
-                      iconSize: 30.0,
-                      icon: const Icon(Icons.home),
-                      color: Colors.white,
-                      hoverColor: const Color.fromARGB(100, 109, 31, 138),
-                      alignment: Alignment.topLeft,
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const HomePage()),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    IconButton(
-                      iconSize: 30.0,
-                      icon: const Icon(Icons.question_mark_rounded),
-                      color: Colors.white,
-                      hoverColor: const Color.fromARGB(100, 109, 31, 138),
-                      alignment: Alignment.topLeft,
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const HomePage()),
-                        );
-                      },
-                    ),
-                    const SizedBox(
-                      width: 30,
-                      height: 30,
-                    ),
-                    IconButton(
-                      iconSize: 30.0,
-                      icon: const Icon(Icons.chat),
-                      color: Colors.white,
-                      hoverColor: const Color.fromARGB(100, 109, 31, 138),
-                      alignment: Alignment.topLeft,
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const HomePage()),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(
-              width: 30,
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Column(
-                  children: [
-                    Row(
-                      children: [
-                        getLeaderboard(),
-                      ],
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ],
+          children: getGameScreenWidget(context),
         ),
       ),
     );
@@ -118,34 +43,72 @@ class _GamePagePageState extends State<GamePage> {
 
 Container getLeaderboard(){
   return Container(
-        width: 200,
-        height: 200,
-        decoration: const BoxDecoration(
-          color: Color.fromARGB(100, 109, 31, 138),
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            getText("Posición", 18, Alignment.topCenter),
-            getText("1. Leo", 16, Alignment.topLeft),
-            getText("2. Jere", 16, Alignment.topLeft),
-            getText("3. Naye", 16, Alignment.topLeft),
-          ]
-        ),
-  );
-}
-
-Align getText(String data, double fontSize, Alignment alignment){
-  return Align(
-    alignment: alignment,
-    child: Padding(
-      padding: const EdgeInsets.all(9.0),
-      child: Text(
-        data, 
-        style: TextStyle(fontSize: fontSize),
-      ),
+    width: 200,
+    height: 200,
+    decoration: const BoxDecoration(
+      color: Color.fromARGB(100, 109, 31, 138),
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        getText("Posición", 18, Alignment.topCenter),
+        getText("1. Leo", 16, Alignment.topLeft),
+        getText("2. Jere", 16, Alignment.topLeft),
+        getText("3. Naye", 16, Alignment.topLeft),
+      ],
     ),
   );
 }
 
+Widget getChildrenWithIcon(BuildContext context, Icon icon, MaterialPageRoute route){
+  return getIconButtonStyle(getSecondaryColor(), 
+    IconButton(
+      icon: icon,
+      iconSize: getIconSize(),
+      alignment: Alignment.center,
+      onPressed: () {
+        Navigator.push(
+          context,
+          route,
+        );
+      },
+    ),
+  );
+}
+
+
+List<Widget> getGameScreenWidget(BuildContext context){
+  return (
+    [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          getChildrenWithIcon(context, const Icon(Icons.home), 
+              MaterialPageRoute(builder: (context) => const HomePage())
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 12.0),
+                child: getChildrenWithIcon(context, const Icon(Icons.question_mark_rounded),
+                  MaterialPageRoute(builder: (context) => const HomePage())
+                ),
+              ),
+              getIconButtonStyle(getSecondaryColor(), openChat(context, getSecondaryColor(), getPrimaryColor()),)
+            ],
+          ),
+        ],
+      ),
+      Padding(
+        padding: const EdgeInsets.only(top: 18.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            getLeaderboard(),
+          ],
+        ),
+      ),
+    ]
+  );
+}
