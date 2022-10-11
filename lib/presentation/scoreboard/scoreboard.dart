1cecf4429e490140a32edbc4df4d8ab79215bc30
Jeremy Vargas Artavia <jeremychaman@gmail.com>
2022-10-10 19:50:39 -0600

    Responsive, modularization and documentation of the code.

--- lib/presentation/scoreboard/scoreboard.dart
+++ lib/presentation/scoreboard/scoreboard.dart
@@ -6,11 +6,12 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:spot_it_game/presentation/core/loading_widget.dart';
 import 'package:spot_it_game/presentation/core/get_children_with_icon.dart';
 import 'package:spot_it_game/presentation/core/button_style.dart';
-
+import 'package:flutter/services.dart';
+import 'package:spot_it_game/presentation/core/size_config.dart';
 import 'package:spot_it_game/presentation/scoreboard/colors.dart';
 
 class ScoreboardPage extends StatefulWidget {
-  static String routeName = '/scoreboard'; // /scoreboard
+  static String routeName = '/scoreboard';
   const ScoreboardPage({Key? key}) : super(key: key);
   @override
   State<ScoreboardPage> createState() => _ScoreboardPageState();
@@ -23,40 +24,41 @@ class _ScoreboardPageState extends State<ScoreboardPage> {
   @override
   void initState() {
     super.initState();
-    addRoom();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    getWinners();
   }
 
-  Future<void> addRoom() async {
+  Future<void> getWinners() async {
     isLoading = false;
   }
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       backgroundColor: getPrimaryColor(),
-      appBar: AppBar(
-        title: const Text('Resultados'),
-        automaticallyImplyLeading: false,
-        backgroundColor: getSecondaryColor(),
-      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
-          child: isLoading ? const LoadingWidget() : const _RoomWidget(),
+          child: isLoading ? const LoadingWidget() : const _ScoreboardWidget(),
         ),
       ),
     );
   }
 }
 
-class _RoomWidget extends StatefulWidget {
-  const _RoomWidget({Key? key}) : super(key: key);
+class _ScoreboardWidget extends StatefulWidget {
+  const _ScoreboardWidget({Key? key}) : super(key: key);
 
   @override
-  State<_RoomWidget> createState() => _RoomWidgetState();
+  State<_ScoreboardWidget> createState() => _ScoreboardWidgetState();
 }
 
-class _RoomWidgetState extends State<_RoomWidget> {
+class _ScoreboardWidgetState extends State<_ScoreboardWidget> {
   final ButtonStyle style = getButtonStyle(650, 85, 30.0, getSecondaryColor());
 
   late List<_ChartData> data;
@@ -64,6 +66,7 @@ class _RoomWidgetState extends State<_RoomWidget> {
 
   @override
   void initState() {
+    // Data for demostration
     data = [
       _ChartData('Sofia', 20),
       _ChartData('Leonel', 30),
@@ -80,62 +83,95 @@ class _RoomWidgetState extends State<_RoomWidget> {
         Flexible(
             flex: 1,
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                getChildrenWithIcon(
-                    context,
-                    const Icon(Icons.home),
-                    getSecondaryColor(),
-                    MaterialPageRoute(builder: (context) => const HomePage())),
-                getChildrenWithIcon(
-                    context,
-                    const Icon(Icons.replay),
-                    getSecondaryColor(),
-                    MaterialPageRoute(builder: (context) => const GamePage())),
-              ],
-            )),
-        getFocusBox(
-            Column(
-              children: [
-                Flexible(
-                  flex: 2,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Text("", style: TextStyle(fontSize: 20.0)),
-                      const Text("Tabla de posiciones",
-                          style: TextStyle(fontSize: 20.0),
-                          textAlign: TextAlign.center),
-                      getChildrenWithIcon(
-                          context,
-                          const Icon(Icons.list),
-                          getSecondaryColor(),
-                          MaterialPageRoute(
-                              builder: (context) => const GamePage())),
-                    ],
-                  ),
+                // Home and replay icons
+                getNavigationButtons(context),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40),
+                  // Main screen
+                  child: getFocusBox(
+                      Column(
+                        children: [
+                          // Title and button of the main screen
+                          getHeader(context),
+                          const Text("", style: TextStyle(fontSize: 40.0)),
+                          // Graph of Columns
+                          getBarChart(_tooltip, data)
+                        ],
+                      ),
+                      SizeConfig.safeBlockVertical * 85,
+                      SizeConfig.safeBlockHorizontal * 60),
                 ),
                 const Text("", style: TextStyle(fontSize: 40.0)),
-                SfCartesianChart(
-                    primaryXAxis: CategoryAxis(),
-                    primaryYAxis:
-                        NumericAxis(minimum: 0, maximum: 30, interval: 10),
-                    tooltipBehavior: _tooltip,
-                    series: <ChartSeries<_ChartData, String>>[
-                      ColumnSeries<_ChartData, String>(
-                          dataSource: data,
-                          xValueMapper: (_ChartData data, _) => data.x,
-                          yValueMapper: (_ChartData data, _) => data.y,
-                          name: 'Puntos',
-                          color: getColumnColor())
-                    ])
               ],
-            ),
-            500,
-            700), //focus box
+            )),
       ],
     );
   }
+}
+
+// @param context: Build context
+// @return Row with home and replay navigation icons
+Row getNavigationButtons(context) {
+  return Row(
+    children: [
+      getChildrenWithIcon(context, const Icon(Icons.home), getSecondaryColor(),
+          MaterialPageRoute(builder: (context) => const HomePage())),
+      getChildrenWithIcon(
+          context,
+          const Icon(Icons.replay),
+          getSecondaryColor(),
+          MaterialPageRoute(builder: (context) => const GamePage())),
+    ],
+  );
+}
+
+// @param context: Build context
+// @return Flexible with tittle and navigation list icon
+Flexible getHeader(context) {
+  return Flexible(
+    flex: 2,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text("", style: TextStyle(fontSize: 20.0)),
+        const Text("Tabla de posiciones",
+            style: TextStyle(fontSize: 20.0), textAlign: TextAlign.center),
+        Padding(
+          padding: const EdgeInsets.only(right: 13, top: 5),
+          child: getChildrenWithIcon(
+              context,
+              const Icon(Icons.list),
+              getSecondaryColor(),
+              MaterialPageRoute(builder: (context) => const GamePage())),
+        ),
+      ],
+    ),
+  );
+}
+
+// @param _tooltip: Component used in Syncfunctions charts
+// @param data: Data to be displayed on the chart
+// @return SizedBox with Column Chart of the data
+SizedBox getBarChart(TooltipBehavior _tooltip, List<_ChartData> data) {
+  return SizedBox(
+    child: SfCartesianChart(
+        primaryXAxis: CategoryAxis(),
+        primaryYAxis: NumericAxis(minimum: 0, maximum: 30, interval: 10),
+        tooltipBehavior: _tooltip,
+        series: <ChartSeries<_ChartData, String>>[
+          ColumnSeries<_ChartData, String>(
+              dataSource: data,
+              xValueMapper: (_ChartData data, _) => data.x,
+              yValueMapper: (_ChartData data, _) => data.y,
+              name: 'Puntos',
+              color: getColumnColor())
+        ]),
+    height: SizeConfig.safeBlockVertical * 50,
+    width: SizeConfig.safeBlockHorizontal * 50,
+  );
 }
 
 class _ChartData {
