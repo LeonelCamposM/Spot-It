b4d512a1530f4046dbf36a18cce424020de20951
Nayeri Azofeifa <NAYERI.AZOFEIFA@ucr.ac.cr>
2022-10-11 22:13:06 -0600

    Added comments

--- lib/presentation/core/text_button_style.dart
+++ lib/presentation/core/text_button_style.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:spot_it_game/presentation/core/text_style.dart';
 import 'button_style.dart';
 
+// @param text: button's text
+// @param width: size for the button's width
+// @param height: size fot the button's height
+// @return the sixed box with the choosen size and text for the button
 SizedBox getTextButton(String text, double width, double height,
     double fontSize, Color color, route, args, context) {
   return SizedBox(
