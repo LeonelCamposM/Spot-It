a9d6ae8f6f318c6def84c9305cadb16c1c10d837
LeonelCamposM <leonelscamposm@gmail.com>
2022-10-22 17:32:06 -0600

    fix apk build

--- lib/application/rooms/rooms_use_case.dart
+++ lib/application/rooms/rooms_use_case.dart
@@ -1,5 +1,3 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:spot_it_game/domain/rooms/i_room_repository.dart';
 import 'package:spot_it_game/domain/rooms/room.dart';
