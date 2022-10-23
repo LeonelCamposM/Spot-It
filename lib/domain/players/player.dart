c63559508bdad2f579a2d67c473d107e2bbb4ddb
Nayeri Azofeifa <NAYERI.AZOFEIFA@ucr.ac.cr>
2022-10-22 20:30:11 -0600

    added player repository to infraestructure and fixed details

--- lib/domain/players/player.dart
+++ lib/domain/players/player.dart
@@ -3,23 +3,23 @@ class Player {
   final String icon;
   final String displayedCard;
   final int cardCount;
-  final int stackCardCount;
+  final int stackCardsCount;
 
   Player(this.nickname, this.icon, this.displayedCard, this.cardCount,
-      this.stackCardCount);
+      this.stackCardsCount);
 
   factory Player.fromJson(Map<String, dynamic> json) => Player(
       json['nickname'] as String,
       json['icon'] as String,
       json["displayedCard"] as String,
       json["cardCount"] as int,
-      json["stackCardCount" as int]);
+      json["stackCardsCount" as int]);
 
   Map<String, dynamic> toJson() => {
         'nickname': nickname,
         'icon': icon,
         "displayedCart": displayedCard,
         "cardCount": cardCount,
-        "stackCardCount": stackCardCount
+        "stackCardsCount": stackCardsCount
       };
 }
