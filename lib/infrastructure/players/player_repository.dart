7798685721e16c99c87572752e955fa6ce2c84dc
Sofia-29 <angiesofiag04@gmail.com>
2022-10-26 15:27:19 -0600

    Add method to do spot it

--- lib/infrastructure/players/player_repository.dart
+++ lib/infrastructure/players/player_repository.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:spot_it_game/domain/players/i_player_repository.dart';
 import 'package:spot_it_game/domain/players/player.dart';
+import 'package:spot_it_game/domain/scoreboard/scoreboard.dart';
 import 'package:spot_it_game/infrastructure/players/eventListeners/on_players_update.dart';
 
 class PlayerRepository implements IPlayerRepository {
@@ -29,13 +30,18 @@ class PlayerRepository implements IPlayerRepository {
   @override
   Future<bool> spotIt(
       String roomID, String playerOneNickname, String playerTwoNickname) async {
+    
     final db = FirebaseFirestore.instance;
-
     var roomReference = db.collection('Room_Player').doc(roomID);
+    var scoreboardReference =
+        db.collection('Room_Scoreboard').doc('jTKFlTMyk0Rw24pdPcmv');
 
     var playersReference = roomReference.collection('players');
+    var playerScoreboardReference = scoreboardReference.collection('Scoreboard');
+
     Player winnerPlayer = Player("", "", "", 0, 0);
     var snapshots = await playersReference.get();
+    var snapshotsScoreboard = await playerScoreboardReference.get();
 
     for (var doc in snapshots.docs) {
       final query = await doc.reference.get();
@@ -51,6 +57,19 @@ class PlayerRepository implements IPlayerRepository {
             0,
             0);
         await doc.reference.update(newPlayer.toJson());
+
+        for (var docScoreboard in snapshotsScoreboard.docs) {
+          final queryScoreboard = await docScoreboard.reference.get();
+          Map<String, dynamic> queryData = queryScoreboard.data()!;
+          if (queryData['nickname'] == playerOneNickname) {
+            Scoreboard winnerScoreboard =
+                Scoreboard(queryData['nickname'], queryData['score']);
+            Scoreboard newScoreboard =
+                Scoreboard(winnerScoreboard.nickname, winnerScoreboard.score + 1);
+            await docScoreboard.reference.update(newScoreboard.toJson());
+            break;
+          }
+        }
         break;
       }
     }
@@ -71,19 +90,6 @@ class PlayerRepository implements IPlayerRepository {
         await doc.reference.update(newPlayer.toJson());
       }
     }
-
-    // await db.runTransaction((transaction) async {
-    //   // final _scoreboardCollection =
-    //   //     db.collection("/Room_Scoreboard/" + roomID + "/Scoreboard");
-    //   DocumentSnapshot players = await transaction.get(roomReference);
-    //   final winnerPlayerReference = await players.get(winnerID);
-    //   final loserPlayer = await players.get(loserID);
-
-    //   transaction.update(roomReference, );
-    // }).then(
-    //   (value) => print("DocumentSnapshot successfully updated!"),
-    //   onError: (e) => print("Error updating document $e"),
-    // );
     return false;
   }
 
